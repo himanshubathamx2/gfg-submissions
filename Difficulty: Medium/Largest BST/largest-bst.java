5ff9e4ace13596e7bbdc09acb20200be40e180d4
Himanshu Batham <112793814+himanshubathamx2@users.noreply.github.com>
2026-08-28 15:39:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Largest BST/largest-bst.java
+++ Difficulty: Medium/Largest BST/largest-bst.java
@@ -10,24 +10,20 @@ class NodeValues{
     }
 }
 
-
 class Solution {
     public int largestBst(Node root) {
-        return f(root).maxSize;    
+        return f(root).maxSize;        
     }
     
     public NodeValues f(Node root){
-        if(root == null){
+        if(root == null)
             return new NodeValues(0, Integer.MIN_VALUE, Integer.MAX_VALUE);
-        }
-        
         NodeValues left = f(root.left);
         NodeValues right = f(root.right);
-        
-        if(left.maxNode <  root.data && right.minNode > root.data){
-            return new NodeValues(1 + left.maxSize + right.maxSize, 
-            Math.max(root.data, right.maxNode), Math.min(root.data, left.minNode));    
+        if(left.maxNode < root.data && root.data < right.minNode){
+            return new NodeValues(left.maxSize + right.maxSize + 1, 
+            Math.max(root.data, right.maxNode), Math.min(root.data, left.minNode));
         }
-        return new NodeValues(Math.max(left.maxSize, right.maxSize), Integer.MAX_VALUE, Integer.MIN_VALUE); 
+        return new NodeValues(Math.max(left.maxSize, right.maxSize), Integer.MAX_VALUE, Integer.MIN_VALUE);
     }
 }
